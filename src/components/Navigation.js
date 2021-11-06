9a183bbae7ae648bb003934c3f8cb3254ffbb0a5
lee7198 <lee7198@naver.com>
2021-11-06 14:40:14 +0900

    1106-2

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -23,7 +23,7 @@ const Navigation = ({ userObj }) => (
           alignItems="center"
         >
           <Grid item xs>
-            <IconButton color="inherit" aria-label="home" href="./">
+            <IconButton color="inherit" aria-label="home" href="/Nwitter/">
               <HomeRoundedIcon />
             </IconButton>
           </Grid>
@@ -39,7 +39,7 @@ const Navigation = ({ userObj }) => (
                 alignItems="center"
               >
                 <Button
-                  href="./profile"
+                  href="/Nwitter/#/profile"
                   variant="text"
                   style={{ color: "#fff" }}
                   endIcon={
