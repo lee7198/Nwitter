2a6fbdaf3a7d85c72a1bea65982316932ee7b2c5
lee7198 <lee7198@naver.com>
2022-03-30 10:44:41 +0900

    animation

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, Component } from "react";
 import { collection, onSnapshot, query, orderBy } from "firebase/firestore";
 import { dbService } from "fbase";
 import Nweet from "components/Nweet";
 import { Box } from "@mui/system";
 import Grid from "@mui/material/Grid";
 import NweetFactory from "components/NweetFactory";
+import ScrollAnimation from "react-animate-on-scroll";
+import "animate.css/animate.min.css";
 
 const Home = ({ userObj }) => {
   const [nweets, setNweets] = useState([]);
@@ -29,14 +31,20 @@ const Home = ({ userObj }) => {
       <NweetFactory userObj={userObj} />
       <Grid container>
         {nweets.map((nweet) => (
+          //nweetCount++;
           <Grid xs={12} md={6} lg={4} xl={4} minHeight="50px" sx={{ px: 1 }}>
-            <h5>{nweetCount++}</h5>
-            <Nweet
-              key={nweet.id}
-              userObj={userObj}
-              nweetObj={nweet}
-              isOwner={nweet.creatorId === userObj.uid}
-            />
+            <ScrollAnimation
+              animateIn="fadeInUp"
+              animateOut="bounceOutLeft"
+              animateOnce={true}
+            >
+              <Nweet
+                key={nweet.id}
+                userObj={userObj}
+                nweetObj={nweet}
+                isOwner={nweet.creatorId === userObj.uid}
+              />
+            </ScrollAnimation>
           </Grid>
         ))}
       </Grid>
